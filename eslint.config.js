bbe960eb5a109bff631ebe1746fbeec2eda5ece3
MelvinCarlo72 <villanuevamelvincarloe@gmail.com>
2025-11-05 20:32:10 +0800

    Migrate ESLint configuration to new format

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+
+export default [
+  {
+    files: ["**/*.js"],
+    ignores: ["dist", "node_modules"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    ...js.configs.recommended,
+    rules: {
+      semi: ["error", "always"],
+      "no-unused-vars": ["warn", { "vars": "all", "args": "after-used" }]
+    }
+  }
+];
